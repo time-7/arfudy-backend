b7af7f8ba7f59ec039905df538a9746fd1323dec
gabrielznegri <gabrielznegri@gmail.com>
2022-12-04 18:32:47 -0300

    Renaming repository

--- src/main/java/com/arfudy/backend/repository/IngredienteRepository.java
+++ src/main/java/com/arfudy/backend/repository/IngredienteRepository.java
@@ -3,8 +3,8 @@ package com.arfudy.backend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.arfudy.backend.entity.Prato;
+import com.arfudy.backend.entity.Ingrediente;
 
 @Repository
-public interface PratoRepository extends JpaRepository<Prato, Integer> {
+public interface IngredienteRepository extends JpaRepository<Ingrediente, Integer> {
 }
