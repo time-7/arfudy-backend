8a31fdc6ea58b75e7b09f3ad76d854bad9aca891
gabrielznegri <gabrielznegri@gmail.com>
2022-12-04 15:19:41 -0300

    Creating new repository

---
+++ src/main/java/com/arfudy/backend/repository/PratoRepository.java
@@ -0,0 +1,10 @@
+package com.arfudy.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.arfudy.backend.entity.Prato;
+
+@Repository
+public interface PratoRepository extends JpaRepository<Prato, Integer> {
+}
