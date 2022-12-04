6366bc49c41dd9992514a62760538518cd4b5977
gabrielznegri <gabrielznegri@gmail.com>
2022-12-04 17:51:08 -0300

    Addding ingrediente repository

--- src/main/java/com/arfudy/backend/repository/IngredienteRepository.java
+++ src/main/java/com/arfudy/backend/repository/IngredienteRepository.java
@@ -1,10 +1,25 @@
 package com.arfudy.backend.repository;
 
+// import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.Query;
+// import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.arfudy.backend.entity.Prato;
+import com.arfudy.backend.entity.Ingrediente;
+
+import jakarta.transaction.Transactional;
 
 @Repository
-public interface PratoRepository extends JpaRepository<Prato, Integer> {
+@Transactional
+public interface IngredienteRepository extends JpaRepository<Ingrediente, Integer> {
+
+//    @Query(nativeQuery = true,
+//            value = "select * from ingrediente as i\n"
+//                    + "join ingrediente_prato ip\n"
+//                    + "on ip.id_ingrediente = i.id_ingrediente\n"
+//                    + "where ip.id_prato in (:id_prato_list)")
+//    List<Ingrediente> findByIdPrato(@Param("id_prato_list") List<Integer> idPratoList);
+
 }
